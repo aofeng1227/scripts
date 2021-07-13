5ce0ba7970758e62783d2756afbdfa2dc3e4163c
XingYunDesign <liujiang19861227@163.com>
2021-07-13 15:37:42 +0800

    更新 jd_jddj_bean.js

--- jd/jd_jddj_bean.js
+++ jd/jd_jddj_bean.js
@@ -13,26 +13,6 @@ TG群:https://t.me/passerbyb2021
 cron "10 0 * * *" script-path=jddj_bean.js,tag=京东到家鲜豆任务
 
 */
-/*
-京东到家鲜豆任务脚本,支持qx,loon,shadowrocket,surge,nodejs
-用抓包抓 https://daojia.jd.com/html/index.html 页面cookie填写到下面,暂时不知cookie有效期
-抓多账号直接清除浏览器缓存再登录新账号,千万别点退出登录,否则cookie失效
-cookie只要里面的deviceid_pdj_jd=xxx-xxx-xxx;o2o_m_h5_sid=xxx-xxx-xxx关键信息
-一天运行一次
-boxjs订阅地址:https://gitee.com/passerby-b/javascript/raw/master/JD/passerby-b.boxjs.json
-TG群:https://t.me/passerbyb2021
-
-
-===================quantumultx================
-[task_local]
-# 京东到家鲜豆任务
-10 0 * * * jddj_bean.js, tag=京东到家鲜豆任务, enabled=true
-=====================Loon================
-[Script]
-cron "10 0 * * *" script-path=jddj_bean.js,tag=京东到家鲜豆任务
- */
-
-
 
 
 const $ = new Env("京东到家鲜豆任务");
