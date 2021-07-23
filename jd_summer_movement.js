8074b347ed3b7cab9315340a36b68c035a23bac8
XingYunDesign <liujiang19861227@163.com>
2021-07-23 14:54:33 +0800

    更新 jd_summer_movement.js

--- jd_summer_movement.js
+++ jd_summer_movement.js
@@ -39,9 +39,9 @@ $.inviteList = [];
 $.secretpInfo = {};
 $.ShInviteList = [];
 $.innerShInviteList = [
-  'H8mphLbwLgz3e4GeFdc0g9GS9KyvaS3S',
-  'H8mphLbwLn_LHtvAULB0thOUapqKwhU',
-  'H8mphLbwLnPnJ8L9XqdUv7O1wfsqrXQ'
+  '',
+  '',
+  ''
 ];
 if ($.isNode()) {
   Object.keys(jdCookieNode).forEach((item) => {
