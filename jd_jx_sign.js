a8aec1aa523301c70808ed6edff89bcb85f98d03
XingYunDesign <liujiang19861227@163.com>
2021-07-29 18:56:10 +0800

    更新 jd_jx_sign.js

--- jd_jx_sign.js
+++ jd_jx_sign.js
@@ -4,9 +4,9 @@
 
 */
 const $ = new Env('京喜签到');
-const notify = $.isNode() ? require('../sendNotify') : '';
+const notify = $.isNode() ? require('./sendNotify') : '';
 //Node.js用户请在jdCookie.js处填写京东ck;
-const jdCookieNode = $.isNode() ? require('../jdCookie.js') : '';
+const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
 let jdNotify = true;//是否关闭通知，false打开通知推送，true关闭通知推送
 //IOS等用户直接用NobyDa的jd cookie
 let cookiesArr = [], cookie = '', message;
